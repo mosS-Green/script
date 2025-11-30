e478cbdf152f405b990f8e177fb3f72d9efdd185
mosS-Green <76946302+mosS-Green@users.noreply.github.com>
2025-11-30 18:23:58 +0530

    ...

--- app/src/main/java/com/example/script/data/local/ScriptDatabase.kt
+++ app/src/main/java/com/example/script/data/local/ScriptDatabase.kt
@@ -3,7 +3,7 @@ package com.example.script.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [BookEntity::class], version = 1)
+@Database(entities = [BookEntity::class], version = 1, exportSchema = false)
 abstract class ScriptDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
 }
